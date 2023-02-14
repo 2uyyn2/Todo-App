07adeba4e52b4905e2365c5cf1c88232699626b3
uyun Lee <2uyyn2@gmail.com>
2023-02-14 14:48:01 +0900

    commit

---
+++ section5-todo-app/src/main/java/com/example/section5todoapp/Section5TodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.section5todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section5TodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section5TodoAppApplication.class, args);
+	}
+
+}
